4c6089645fd2003950adc553b2003a4a2daa9454
Wilson Montoya <wichomontoya@gmail.com>
2021-09-01 05:45:05 -0500

    Fix points to user

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -52,7 +52,7 @@ class Api::V1::UsersController < ApplicationController
 
   def get_points
     user = User.find(params[:id])
-    total_points = user.purchaces.map{|x| x.total}.inject(0){|sum,x| sum + x }
+    total_points = (user.purchaces.map{|x| x.total}.inject(0){|sum,x| sum + x } / 10000)
     spent_points = user.purchaces.select{|x| x.offer_id != nil}.map{|x| Offer.find(x.offer_id).points}.inject(0){|sum,x| sum + x }
     user.update(total_points: total_points, spent_points: spent_points)
     current_points = user.total_points - user.spent_points
