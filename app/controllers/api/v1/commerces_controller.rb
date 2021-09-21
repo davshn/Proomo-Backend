4b071e0800b6306ee2bdedf876f5767b9eef2f51
Wilson Montoya <wichomontoya@gmail.com>
2021-09-21 06:12:04 -0500

    Ajust report with commision

--- app/controllers/api/v1/commerces_controller.rb
+++ app/controllers/api/v1/commerces_controller.rb
@@ -124,7 +124,7 @@ class Api::V1::CommercesController < ApplicationController
       reports = []
       commerces = Commerce.all
       commerces.each do |commerce|
-        reports << Purchace.where(commerce_id: commerce.id, updated_at: date_active).map{|x| [commerce.name, commerce.city, commerce.partner.documment, commerce.partner.name, (x.client.first_name + ' ' + x.client.last_name), x.client.email, x.client.phone, x.ticket_id, x.state, x.total, x.created_at, commerce.nit ? commerce.nit : '']}
+        reports << Purchace.where(commerce_id: commerce.id, updated_at: date_active).map{|x| [commerce.name, commerce.city, commerce.partner.documment, commerce.partner.name, (x.client.first_name + ' ' + x.client.last_name), x.client.email, x.client.phone, x.ticket_id, x.state, x.total, x.created_at, commerce.nit ? commerce.nit : '', (x.total * 0.06)]}
       end
       @reports = reports.reject(&:empty?)
       render_json(
