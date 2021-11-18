e82189199d34a0e37fce52bd04a439acfc8ff7d5
Wilson Montoya <wichomontoya@gmail.com>
2021-11-18 15:43:46 -0500

    Fix error in calculate current points

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -52,8 +52,8 @@ class Api::V1::UsersController < ApplicationController
 
   def get_points
     user = User.find(params[:id])
-    total_points = (user.purchaces.map{|x| x.total}.inject(0){|sum,x| sum + x } / 10000)
-    spent_points = user.purchaces.select{|x| x.offer_id != nil}.map{|x| Offer.find(x.offer_id).points}.inject(0){|sum,x| sum + x }
+    total_points = (user.purchaces.where(return_code:"OK").map{|x| x.total}.inject(0){|sum,x| sum + x } / 10000)
+    spent_points = user.purchaces.where(return_code:"OK").select{|x| x.offer_id != nil}.map{|x| Offer.find(x.offer_id).points}.inject(0){|sum,x| sum + x }
     user.update(total_points: total_points, spent_points: spent_points)
     current_points = user.total_points - user.spent_points
     render json: { current_points: current_points }, status: 200
