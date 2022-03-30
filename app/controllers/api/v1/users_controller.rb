0942d4a3ea401d60d8f3c687c21385faf331b208
Wilson Montoya <wichomontoya@gmail.com>
2022-03-30 06:38:00 -0500

    Include user errors

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -8,7 +8,13 @@ class Api::V1::UsersController < ApplicationController
           status: 200
       )
     else
-      render json: { errors: @user.errors }, status: 422
+      if @user == nil
+        render json: { errors: "No se encuentro el usurio en nuesto sistema" }, status: 422
+      elsif @user.valid_password?(params[:data][:attributes][:password])
+        render json: { errors: "La contraseña no corresponde a este usuario" }, status: 422
+      elsif @user.has_role?(:client)
+        render json: { errors: "Este usuario no es valido" }, status: 422
+      end
     end
   end
 
@@ -20,7 +26,13 @@ class Api::V1::UsersController < ApplicationController
           status: 200
       )
     else
-      render json: { errors: @user.errors }, status: 422
+      if @user == nil
+        render json: { errors: "No se encuentro el usurio en nuesto sistema" }, status: 422
+      elsif @user.valid_password?(params[:data][:attributes][:password])
+        render json: { errors: "La contraseña no corresponde a este usuario" }, status: 422
+      elsif @user.has_role?(:client)
+        render json: { errors: "Este usuario no es valido" }, status: 422
+      end
     end
   end
 
